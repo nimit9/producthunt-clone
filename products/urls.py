3d6837ef95f231d6b1cac06f2d6ef09250c713c2
Nimit Haria <nimitharia007@gmail.com>
2019-04-20 20:52:39 +0530

    Fourth commit

---
+++ products/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from products import views
+
+urlpatterns = [
+    path('create', views.create, name='create'),
+    path('<int:product_id>', views.detail, name='detail'),
+    ]
